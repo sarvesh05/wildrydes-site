aece2ecf44415a0ef7382d8748cd6f9429ad59d9
Sarvesh Rawat <sarveshrawat@Sarveshs-MacBook-Air.local>
2020-08-16 13:11:33 +0530

    adding new app client from cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_1A67dOCc7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3hcmjar5qh5t6hvcvb7lljo13g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_sohuY1ZH6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2uulkqo9s5nan8s6gisqiucq3q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
