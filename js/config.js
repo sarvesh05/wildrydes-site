4c33ffa6b50b868b2f378587cb55c29a85d554e9
Sarvesh Rawat <sarveshrawat@Sarveshs-MacBook-Air.local>
2020-08-16 12:58:43 +0530

    Configuring the cognito in the app

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_1A67dOCc7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3hcmjar5qh5t6hvcvb7lljo13g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
